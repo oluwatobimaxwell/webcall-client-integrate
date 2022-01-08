a2e6ae19b1457875637d89ea0c06c3d538a67ec5
oluwatobimaxwell <39818137+oluwatobimaxwell@users.noreply.github.com>
2022-01-08 17:40:47 +0100

    Update and rename webcall-plugin@1.0.3.js to webcall-plugin@1.0.4.js

--- webcall-plugin@1.0.4.js
+++ webcall-plugin@1.0.4.js
@@ -1,11 +1,19 @@
+
+const serverBase = `https://evokeeclinic.herokuapp.com`;
+const server = "https://webcaller.netlify.app?";
+// const server = `http://172.20.10.4:3000?`;
+
 const fontLink =
 	"https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap";
 const iconLink =
 	"cdn.jsdelivr.net/npm/@mdi/font@6.5.95/css/materialdesignicons.min.css";
 
 const scripts = [
+	"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
 	"https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js",
-	"https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+	"https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js",
+	"https://html2canvas.hertzen.com/dist/html2canvas.min.js",
+	"https://unpkg.com/imagekit-javascript/dist/imagekit.min.js"
 ]
 
 const appendStyle = (styleToAppend) => {
@@ -32,8 +40,190 @@ const getTabId = () => {
 	return iPageTabID;
 }
 
-const server = "https://webcaller.netlify.app?";
-// const server = "http://172.20.10.4:3000?";
+
+// returns a new canvas containing the scaled image.
+function downScaleCanvas(cv, scale) {
+    if (!(scale < 1) || !(scale > 0)) throw ('scale must be a positive number <1 ');
+    var sqScale = scale * scale; // square scale = area of source pixel within target
+    var sw = cv.width; // source image width
+    var sh = cv.height; // source image height
+    var tw = Math.floor(sw * scale); // target image width
+    var th = Math.floor(sh * scale); // target image height
+    var sx = 0, sy = 0, sIndex = 0; // source x,y, index within source array
+    var tx = 0, ty = 0, yIndex = 0, tIndex = 0; // target x,y, x,y index within target array
+    var tX = 0, tY = 0; // rounded tx, ty
+    var w = 0, nw = 0, wx = 0, nwx = 0, wy = 0, nwy = 0; // weight / next weight x / y
+    // weight is weight of current source point within target.
+    // next weight is weight of current source point within next target's point.
+    var crossX = false; // does scaled px cross its current px right border ?
+    var crossY = false; // does scaled px cross its current px bottom border ?
+    var sBuffer = cv.getContext('2d').
+    getImageData(0, 0, sw, sh).data; // source buffer 8 bit rgba
+    var tBuffer = new Float32Array(3 * tw * th); // target buffer Float32 rgb
+    var sR = 0, sG = 0,  sB = 0; // source's current point r,g,b
+    /* untested !
+    var sA = 0;  //source alpha  */    
+
+    for (sy = 0; sy < sh; sy++) {
+        ty = sy * scale; // y src position within target
+        tY = 0 | ty;     // rounded : target pixel's y
+        yIndex = 3 * tY * tw;  // line index within target array
+        crossY = (tY != (0 | ty + scale)); 
+        if (crossY) { // if pixel is crossing botton target pixel
+            wy = (tY + 1 - ty); // weight of point within target pixel
+            nwy = (ty + scale - tY - 1); // ... within y+1 target pixel
+        }
+        for (sx = 0; sx < sw; sx++, sIndex += 4) {
+            tx = sx * scale; // x src position within target
+            tX = 0 |  tx;    // rounded : target pixel's x
+            tIndex = yIndex + tX * 3; // target pixel index within target array
+            crossX = (tX != (0 | tx + scale));
+            if (crossX) { // if pixel is crossing target pixel's right
+                wx = (tX + 1 - tx); // weight of point within target pixel
+                nwx = (tx + scale - tX - 1); // ... within x+1 target pixel
+            }
+            sR = sBuffer[sIndex    ];   // retrieving r,g,b for curr src px.
+            sG = sBuffer[sIndex + 1];
+            sB = sBuffer[sIndex + 2];
+
+            /* !! untested : handling alpha !!
+               sA = sBuffer[sIndex + 3];
+               if (!sA) continue;
+               if (sA != 0xFF) {
+                   sR = (sR * sA) >> 8;  // or use /256 instead ??
+                   sG = (sG * sA) >> 8;
+                   sB = (sB * sA) >> 8;
+               }
+            */
+            if (!crossX && !crossY) { // pixel does not cross
+                // just add components weighted by squared scale.
+                tBuffer[tIndex    ] += sR * sqScale;
+                tBuffer[tIndex + 1] += sG * sqScale;
+                tBuffer[tIndex + 2] += sB * sqScale;
+            } else if (crossX && !crossY) { // cross on X only
+                w = wx * scale;
+                // add weighted component for current px
+                tBuffer[tIndex    ] += sR * w;
+                tBuffer[tIndex + 1] += sG * w;
+                tBuffer[tIndex + 2] += sB * w;
+                // add weighted component for next (tX+1) px                
+                nw = nwx * scale
+                tBuffer[tIndex + 3] += sR * nw;
+                tBuffer[tIndex + 4] += sG * nw;
+                tBuffer[tIndex + 5] += sB * nw;
+            } else if (crossY && !crossX) { // cross on Y only
+                w = wy * scale;
+                // add weighted component for current px
+                tBuffer[tIndex    ] += sR * w;
+                tBuffer[tIndex + 1] += sG * w;
+                tBuffer[tIndex + 2] += sB * w;
+                // add weighted component for next (tY+1) px                
+                nw = nwy * scale
+                tBuffer[tIndex + 3 * tw    ] += sR * nw;
+                tBuffer[tIndex + 3 * tw + 1] += sG * nw;
+                tBuffer[tIndex + 3 * tw + 2] += sB * nw;
+            } else { // crosses both x and y : four target points involved
+                // add weighted component for current px
+                w = wx * wy;
+                tBuffer[tIndex    ] += sR * w;
+                tBuffer[tIndex + 1] += sG * w;
+                tBuffer[tIndex + 2] += sB * w;
+                // for tX + 1; tY px
+                nw = nwx * wy;
+                tBuffer[tIndex + 3] += sR * nw;
+                tBuffer[tIndex + 4] += sG * nw;
+                tBuffer[tIndex + 5] += sB * nw;
+                // for tX ; tY + 1 px
+                nw = wx * nwy;
+                tBuffer[tIndex + 3 * tw    ] += sR * nw;
+                tBuffer[tIndex + 3 * tw + 1] += sG * nw;
+                tBuffer[tIndex + 3 * tw + 2] += sB * nw;
+                // for tX + 1 ; tY +1 px
+                nw = nwx * nwy;
+                tBuffer[tIndex + 3 * tw + 3] += sR * nw;
+                tBuffer[tIndex + 3 * tw + 4] += sG * nw;
+                tBuffer[tIndex + 3 * tw + 5] += sB * nw;
+            }
+        } // end for sx 
+    } // end for sy
+	// create result canvas
+	var resCV = document.createElement('canvas');
+	resCV.width = tw;
+	resCV.height = th;
+	var resCtx = resCV.getContext('2d');
+	var imgRes = resCtx.getImageData(0, 0, tw, th);
+	var tByteBuffer = imgRes.data;
+	// convert float32 array into a UInt8Clamped Array
+	var pxIndex = 0; //  
+	for (sIndex = 0, tIndex = 0; pxIndex < tw * th; sIndex += 3, tIndex += 4, pxIndex++) {
+		tByteBuffer[tIndex] = Math.ceil(tBuffer[sIndex]);
+		tByteBuffer[tIndex + 1] = Math.ceil(tBuffer[sIndex + 1]);
+		tByteBuffer[tIndex + 2] = Math.ceil(tBuffer[sIndex + 2]);
+		tByteBuffer[tIndex + 3] = 255;
+	}
+	// writing result to canvas.
+	resCtx.putImageData(imgRes, 0, 0);
+	return resCV;
+}
+function screenshot(element, options = {}) {
+	// our cropping context
+	let cropper = document.createElement('canvas').getContext('2d');
+	// save the passed width and height
+	let finalWidth = options.width || window.screen.availWidth;
+	let finalHeight = options.height || window.screen.availHeight;
+	// update the options value so we can pass it to h2c
+	if (options.x) {
+	  options.width = finalWidth + options.x;
+	}
+	if (options.y) {
+	  options.height = finalHeight + options.y;
+	}
+	// chain h2c Promise
+	return html2canvas(element, options).then(c => {
+	  // do our cropping
+	  c = downScaleCanvas(c, 0.4);
+	  cropper.canvas.width = c.width || finalWidth;
+	  cropper.canvas.height = ((c.width || finalWidth) * finalHeight)/finalWidth;
+	  cropper.drawImage(c, -(+options.x || 0), -(+options.y || 0));
+	  cropper.scale(0.25, 0.25)
+	  // return our canvas
+	  return cropper.canvas;
+	});
+  }   
+
+const getScreenShot = (name, callback) => {
+	
+	$.getJSON(`${serverBase}/api/v1/image-kit-init/${name}/`, function(data){
+		if(data?.preview){
+			return callback && callback(data?.preview)
+		}
+	}).fail(function(){
+		screenshot(document["body"], {
+			x: 0, // this are our custom x y properties
+			y: 0, 
+			useCORS: true // you can still pass default html2canvas options
+		  }).then(canvas => {
+			//do whatever with the canvas
+				var imagekit = new ImageKit({
+					publicKey : "public_SEtloDbAiC862qTHtVictrKhvYQ=",
+					urlEndpoint : "https://ik.imagekit.io/cmu8v8879kr",
+					authenticationEndpoint : `${serverBase}/api/v1/image-kit-init/initialize/`,
+				});  
+				const base64image = canvas.toDataURL("image/png");
+				imagekit.upload({
+					file : base64image,
+					fileName : name+".jpg",
+					tags : ["tag1"]
+				}, function(err, result) {
+					$.post(`${serverBase}/api/v1/image-kit-init/`, { hostname: name, preview: result.url }, function(data) {
+						callback && callback(result.url);
+					});
+				})
+		  })
+	})
+}
+
+
 
 const getImageUrl = () => {
 	const images = document.getElementsByTagName("img");
@@ -82,8 +272,8 @@ class WebCallClient {
 	}
 
 	init({ clientId }) {
-
-		scripts.forEach(e => {appendScript(e)});
+		this.hostname = window.location.hostname.replace(/\./g, "")
+		scripts.forEach(e => { appendScript(e); });
 
 		this.serverUrl = server;
 		this.transitionTime = 0.5;
@@ -112,7 +302,8 @@ class WebCallClient {
 				currentPage: `${window.location.origin}${window.location.pathname}`,
 				defaultImage: getImageUrl(),
 				pageTitle: document.title,
-				tabId: getTabId()
+				tabId: getTabId(),
+				hostname: this.hostname
 			};
 			this.params = new URLSearchParams(params).toString();
 
@@ -329,6 +520,7 @@ class WebCallClient {
 	messageHandler = (msg) => {
 		switch (msg?.action) {
 			case "load-completed":
+				getScreenShot(this.hostname);
 				clearInterval(this.loading);
 				this.progress.style.width = "100%";
 				setTimeout(() => {
@@ -362,4 +554,6 @@ function setUpView() {
 	}
 }
 
-setUpView();
+document.addEventListener("DOMContentLoaded", function() {
+	setUpView();
+  });
